a55daad8f559e74a80e8aa153df488f10a6548c8
U-C-S <uchanakyasrinivas@gmail.com>
2023-08-22 19:10:23 +0530

    add tile methods

--- app/src/main/java/com/toolsfrom3721/browserqtile/QTileService.kt
+++ app/src/main/java/com/toolsfrom3721/browserqtile/QTileService.kt
@@ -3,6 +3,7 @@ package com.toolsfrom3721.browserqtile
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import android.service.quicksettings.Tile
 import android.service.quicksettings.TileService
 
 class QTileService : TileService() {
@@ -10,6 +11,35 @@ class QTileService : TileService() {
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
     }
+
+    // Called when the user taps on your tile in an active or inactive state.
+    override fun onClick() {
+        super.onClick()
+
+        this.startActivityAndCollapse(Intent(android.provider.Settings.ACTION_MANAGE_DEFAULT_APPS_SETTINGS))
+    }
+
+    // Called when the user adds your tile.
+    override fun onTileAdded() {
+        super.onTileAdded()
+
+        this.qsTile.state = Tile.STATE_ACTIVE
+    }
+    // Called when your app can update your tile.
+    override fun onStartListening() {
+        super.onStartListening()
+    }
+
+    // Called when your app can no longer update your tile.
+    override fun onStopListening() {
+        super.onStopListening()
+    }
+
+    // Called when the user removes your tile.
+    override fun onTileRemoved() {
+        super.onTileRemoved()
+    }
+
 
 
 }
